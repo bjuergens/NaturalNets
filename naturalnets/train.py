5530ad1a839185456267426c5536aeaf85fec1ab
bjoern <git@wotanii.de>
2021-11-05 10:35:30 +0100

    simplified commentary

--- naturalnets/train.py
+++ naturalnets/train.py
@@ -30,54 +30,34 @@ class TrainingCfg:
 
 
 def train(configuration, results_directory):
-
     pool = multiprocessing.Pool()
-
     config = TrainingCfg(**configuration)
-
-    # Get environment class from configuration
     environment_class = get_environment_class(config.environment['type'])
-
-    # Get brain class from configuration
     brain_class = get_brain_class(config.brain['type'])
-
-    # Initialize episode runner
     ep_runner = EpisodeRunner(env_class=environment_class,
                               env_configuration=config.environment,
                               brain_class=brain_class,
                               brain_configuration=config.brain)
-
     individual_size = ep_runner.get_individual_size()
 
     print("Free parameters: " + str(ep_runner.get_free_parameter_usage()))
     print("Individual size: {}".format(individual_size))
     print("Used CPU for training: " + get_cpu_info()["brand_raw"])
 
-    # Get optimizer class from configuration
     optimizer_class = get_optimizer_class(config.optimizer['type'])
-
     opt = optimizer_class(individual_size=individual_size, configuration=config.optimizer)
 
     best_genome_overall = None
     best_reward_overall = -math.inf
-
     start_time_training = time.time()
     start_date_training = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
     log = []
 
-    # Run evolutionary training for given number of generations
+    # Begin NE training loop
     for generation in range(config.number_generations):
-
         start_time_current_generation = time.time()
-
-        # Environment seed for this generation (excludes validation environment seeds)
         env_seed = random.randint(config.number_validation_runs, config.maximum_env_seed)
-
-        # Ask optimizers for new population
         genomes = opt.ask()
-
-        # Training runs for candidates
         evaluations = []
         for genome in genomes:
             evaluations.append([genome, env_seed, config.number_rounds])
@@ -87,9 +67,7 @@ def train(configuration, results_directory):
         # Use this for debugging
         # rewards_training = [ep_runner.eval_fitness(individual_eval) for individual_eval in evaluations]
 
-        # Tell optimizers new rewards
         opt.tell(rewards_training)
-
         best_genome_current_generation = genomes[np.argmax(rewards_training)]
 
         # Validation runs for best individual
@@ -134,12 +112,11 @@ def train(configuration, results_directory):
 
     print("Elapsed time for training: %.2f seconds" % elapsed_time)
 
-    # Create new directory to store data of current training run
+    # write results to output directory
     results_subdirectory = os.path.join(results_directory, start_date_training)
     os.makedirs(results_subdirectory)
     print("Output directory: " + str(results_subdirectory))
 
-    # Save configuration
     with open(os.path.join(results_subdirectory, 'Configuration.json'), 'w') as outfile:
         json.dump(configuration, outfile, ensure_ascii=False, indent=4)
 
@@ -149,7 +126,7 @@ def train(configuration, results_directory):
     # Save brain state (i.e. masks)
     ep_runner.save_brain_state(os.path.join(results_subdirectory, 'Brain_State'))
 
-    # Last element of log contains additional for training
+    # Last element of log contains additional info from training
     log_info = dict()
     log_info["elapsed_time_training"] = elapsed_time
     log_info["cpu"] = get_cpu_info()["brand_raw"]
@@ -158,7 +135,7 @@ def train(configuration, results_directory):
     # Write log to JSON for better parsing
     with open(os.path.join(results_subdirectory, 'Log.json'), 'w') as outfile:
         json.dump(log, outfile, ensure_ascii=False, indent=4)
-        
+
     # Write results to text file for better readibility
     write_results_to_textfile(path=os.path.join(results_subdirectory, 'Log.txt'),
                               configuration=configuration,
@@ -171,3 +148,9 @@ def train(configuration, results_directory):
     # Error messages inside subprocesses could be shown once they are joined
     pool.close()
     pool.join()
+
+
+if __name__ == '__main__':
+    with open("Configuration.json", "r") as readfile:
+        configuration = json.load(readfile)
+    train(configuration=configuration, results_directory="results")
